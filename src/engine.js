5a56bdc473491c5a430a3414e26132cce404dba9
Gabriel Crepaldi <gabrielgcrepaldi@gmail.com>
2025-03-28 14:23:20 -0300

    Improving base Index and CSS

---
+++ src/engine.js
@@ -0,0 +1,16 @@
+let isRunning = false;
+let hierarchy = [];
+
+// Create Game Object, with name, position, rotation, size, isEnabled, components
+
+function AddObjectToHierarchy(object, position, rotation, scale){
+    return object // Return the object reference inside the hierarchy
+}
+
+function RemoveObjectFromHierarchy(id){} // Free memory please
+
+function StartGame(){}
+
+function StopGame(){}
+
+
